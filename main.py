b68ebcb2d980c4f4e9e97fc3674ca3a4e4781563
Quentin Sonnefraud <quentin.sonnefraud@protonmail.com>
2019-09-05 15:36:28 +0200

    [FIX]: better error messages and name for running option

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import argparse
 if __name__ == '__main__':
     import sys
     parser = argparse.ArgumentParser()
-    parser.add_argument('--walks', type=argparse.FileType('r'))
+    parser.add_argument('--running', type=argparse.FileType('r'))
     args, left = parser.parse_known_args()
     sys.argv = sys.argv[:1]+left
-    walks =  args.walks.read()
-    print(walks[0:100])
-    websocket_server.start_app(walks)
+    if not args.running:
+       raise argparse.ArgumentTypeError("Missing option running file")
+    running =  args.running.read()
+    websocket_server.start_app(running)
