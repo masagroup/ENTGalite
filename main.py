32aed163c0324b2352b7a00aecc20474a67cfb30
Quentin Sonnefraud <quentin.sonnefraud@protonmail.com>
2019-09-05 15:38:26 +0200

    [FIX]: pep08 styling

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--running', type=argparse.FileType('r'))
     args, left = parser.parse_known_args()
-    sys.argv = sys.argv[:1]+left
+    sys.argv = sys.argv[:1] + left
     if not args.running:
-       raise argparse.ArgumentTypeError("Missing option running file")
-    running =  args.running.read()
+        raise argparse.ArgumentTypeError("Missing option running file")
+    running = args.running.read()
     websocket_server.start_app(running)
--- simulation_client/websocket_server.py
+++ simulation_client/websocket_server.py
@@ -20,19 +20,23 @@ P = pysw.client_protocols
 
 WALKS = None
 
+
 def send_train_to_interface(train: str):
     """Send Time to interface in order to synchronize simulation and interface"""
     eel.update_train_js(train)
 
+
 def send_time_to_interface(date: str):
     """Send Time to interface in order to synchronize simulation and interface"""
     eel.update_sim_time_js(date)
+
 
 @eel.expose
 def receive_walks_from_py() -> str:
     """Send walks json to interface """
     print(WALKS[0:100])
     return WALKS
+
 
 class TrainProtocol(P.SWLoginP, P.TickPrinterMixin, P.UnitMixin):
     """
@@ -41,9 +45,10 @@ class TrainProtocol(P.SWLoginP, P.TickPrinterMixin, P.UnitMixin):
     peut être pllus clair ensuite.
     """
 
-
     def is_train(self, unit):
-        return isinstance(unit, self.bdd.pawns.TRAIN_Tgv) or isinstance(unit, self.bdd.pawns.TRAIN_Diesel)
+        return isinstance(
+            unit, self.bdd.pawns.TRAIN_Tgv) or isinstance(
+            unit, self.bdd.pawns.TRAIN_Diesel)
 
     def OnReceived_ControlSendCurrentStateBegin(self, msg):
         self.bdd = self.factory.physical_base
@@ -51,39 +56,44 @@ class TrainProtocol(P.SWLoginP, P.TickPrinterMixin, P.UnitMixin):
     def OnReceived_ControlSendCurrentStateEnd(self, msg):
         super().OnReceived_ControlSendCurrentStateEnd(msg)
         U = self.units_by_id
-        self.trains_by_id_sim = dict([(i,U[i]) for i in U \
-                                      if self.is_train(U[i]) ])
+        self.trains_by_id_sim = dict([(i, U[i]) for i in U
+                                      if self.is_train(U[i])])
 
     def OnReceived_UnitCreation(self, msg):
         super().OnReceived_UnitCreation(msg)
         u = self.units_by_id[msg.unit.id]
-        if not self.is_train(u): return
+        if not self.is_train(u):
+            return
         self.OnCreate_train(u)
 
     def OnReceived_UnitAttributes(self, msg):
         super().OnReceived_UnitAttributes(msg)
         u = self.units_by_id[msg.unit.id]
-        if not self.is_train(u): return
+        if not self.is_train(u):
+            return
         self.OnUpdate_train(u)
 
     def OnReceived_UnitDestruction(self, msg):
         u = self.units_by_id[msg.unit.id]
         super().OnReceived_UnitDestruction(msg)
-        if not self.is_train(u): return
+        if not self.is_train(u):
+            return
         self.OnDelete_train(u)
 
     def OnCreate_train(self, created_train):
         pass
+
     def OnUpdate_train(self, updated_train):
         pass
+
     def OnDelete_train(self, deleted_train):
         pass
+
 
 class TestProtocol(TrainProtocol):
 
     def OnReceived_ControlBeginTick(self, msg):
         send_time_to_interface(msg.date_time.data)
-
 
     def OnReceived_ControlEndTick(self, msg):
         pass
@@ -93,14 +103,18 @@ class TestProtocol(TrainProtocol):
     def OnUpdate_train(self, train):
         lat, lon = train.position
 
-        send_train_to_interface(' '.join([train.name, str(train.speed), str(lat), str(lon)]))
-        print("Train '%s': vitesse = %f, pos = (%f,%f)" % (\
+        send_train_to_interface(
+            ' '.join([train.name, str(train.speed), str(lat), str(lon)]))
+        print("Train '%s': vitesse = %f, pos = (%f,%f)" % (
             train.name, train.speed, lat, lon))
+
 
 class TestFactory(P.SWFactory):
     protocol = TestProtocol
+
     def __init__(self, phybdd, login):
         super().__init__(phybdd, login, protocol=TestProtocol)
+
 
 def start_connection(config):
     asyncio.set_event_loop(asyncio.new_event_loop())
@@ -109,6 +123,7 @@ def start_connection(config):
     import sncf
     F = TestFactory(sncf, config.login)
     pysw.app.connect(config, sim_factory=F, timeline_factory=None)
+
 
 def start_app(walks):
     """Start Eel with either production or development configuration."""
@@ -144,10 +159,10 @@ def start_app(walks):
         eel.start(page, mode=app, **eel_kwargs)
     except EnvironmentError:
         # If Chrome isn't found, fallback to Microsoft Edge on Win10 or greater
-        if sys.platform in ['win32', 'win64'] and int(platform.release()) >= 10:
+        if sys.platform in [
+                'win32', 'win64'] and int(
+                platform.release()) >= 10:
             eel.start(page, mode='edge', **eel_kwargs)
         else:
             raise
     t.join()
-
-
